79c98ed6edb42751377f67637a71ffd94abdf7e1
Kate Hodesdon <kate@hodesdon.com>
2016-10-04 23:48:28 +0100

    New classes for evaluating truth values

---
+++ truthvalues.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+
+# Classes for truth functions of propositional logic expressions
+
+class Simple_exp(object)
+    """
+    A simple expression, i.e. a propositional variable.
+    
+    Define value(self) when creating the propositional variable.
+    """
+    def__init__(self):
+        pass
+
+    
+
+class Not_exp(object):
+    """
+    A expression that is a negation, i.e. negation has widest scope.
+    """
+    def __init__(self, rhs):
+        self.rhs = rhs
+
+    def value(self):
+        if value(self.rhs) == True:
+            return False
+        if value(self.rhs) == False:
+            return True
+
+
+class Or_exp(object):
+    """
+    A disjunction: p OR q
+    """
+    def __init__(self, lhs, rhs):
+        self.lhs = lhs
+        self.rhs = rhs
+
+    def value(self):
+        return (value(lhs) or value(rhs))
+
+
+class And_exp(object):
+    """
+    A conjunction: p AND q
+    """
+    def __init__(self, lhs, rhs):
+        self.lhs = lhs
+        self.rhs = rhs
+
+    def value(self):
+        return (value(lhs) and value(rhs))
+        
