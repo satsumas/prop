a10f2bb0f2fbd69a27dc23cfc0b905dc8335a264
Kate Hodesdon <kate@hodesdon.com>
2016-10-08 20:25:17 +0100

    rewriting to use Sympy, leaving out qtree for now

--- tree.py
+++ tree.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 
 from qtree import Qtree
+from sympy import *
+from sympy.core import symbols
+from sympy.logic.boolalg import Or, And, Not, Implies, Equivalent
 import copy
 
 class TraversibleExpression(object):
@@ -10,7 +13,6 @@ class TraversibleExpression(object):
     """
     def __init__(self):
         self.stack = []
-
 
     def getSubTree(self, elideRoot=False):
         """
@@ -38,7 +40,7 @@ class TraversibleExpression(object):
         return myQtree
 
 
-class Not(TraversibleExpression):
+class Not_exp(TraversibleExpression):
     """
     NOT expression.  Can have one sub-expression.
     """
@@ -49,27 +51,17 @@ class Not(TraversibleExpression):
         TraversibleExpression.__init__(self)
         self.sub = sub
 
-
     def render(self):
         return "Not(%s)" % (self.sub.render(),)
-        #return "NOT " + self.sub.render()
-
-    def branch(self):
-        return r"%s" % (self.render())
-
-    def qtree(self, elideRoot=False):
-        return Qtree(self.render())
-
-    def punt(self, stuff):
-        print "Not(%s) punt(%s), extending own stack %s" % (
-            self.render(), 
-	    [s.render() for s in stuff], 
-	    [s.render() for s in self.stack],
-        )
-        self.stack.extend(stuff)
+        #return "NOT " + self.sub.render() 
+  
+    def sympy_me(self, sub):
+        # Model this on the recursive render function
+        return ~(sympy_me(sub))
 
 
-class Or(TraversibleExpression):
+
+class Or_exp(TraversibleExpression):
     """
     OR expression.  Can have sub-expressions.
     """
@@ -80,33 +72,15 @@ class Or(TraversibleExpression):
         self.lhs = lhs
         self.rhs = rhs
 
-
     def render(self):
         return "Or(%s, %s)" % (self.lhs.render(), self.rhs.render())
         #return "(" + self.lhs.render() + " OR " + self.rhs.render() + ")"
+   
+    def sympy_me(self, lhs, rhs):
+        return sympy_me(lhs) | sympy_me(rhs)
 
 
-    def qtree(self, elideRoot=False):
-        if elideRoot:
-            text = ""
-        else:
-            text = self.render()
-        return Qtree(text, [self.lhs.getSubTree(), self.rhs.getSubTree()])
-
-
-    def punt(self, stuff):
-        print "Or(%s) punt(%s), extending lhs stack %s and rhs stack %s, my stack is %s" % (
-            self.render(), [s.render() for s in stuff],
-	    [s.render() for s in self.lhs.stack],
-	    [s.render() for s in self.rhs.stack],
-	    [s.render() for s in self.stack],
-        )
-        self.lhs.stack.extend(stuff)
-        self.rhs.stack.extend(stuff)
-
-
-
-class And(TraversibleExpression):
+class And_exp(TraversibleExpression):
     """
     AND expression.  Can have sub-expressions.
     """
@@ -117,46 +91,11 @@ class And(TraversibleExpression):
         self.lhs = lhs
         self.rhs = rhs
 
-
     def render(self):
         return "And(%s, %s)" % (self.lhs.render(), self.rhs.render())
 
-
-    def _newlineHead(self):
-        return "%s\n%s" % (self.lhs.render(), self.rhs.render())
-
-
-    def qtree(self, elideRoot=False):
-        """
-        For And expressions, render a subtree with each of the conjuncts
-        separated by a newline as the head, and then proceed to recursively
-        expand each conjunct in turn, one at a time.
-        """
-        if self.lhs.isComplex and self.rhs.isComplex:
-            self.lhs.stack.append(self.rhs)
-
-        subQtreeBranches = []
-        if self.lhs.isComplex:
-            subQtreeBranches.append(self.lhs.getSubTree(elideRoot=True))
-        elif self.rhs.isComplex:
-            subQtreeBranches.append(self.rhs.getSubTree(elideRoot=True))
-
-        innerQtree = Qtree(self._newlineHead(), subQtreeBranches)
-        if elideRoot:
-            return innerQtree
-        else:
-            return Qtree(self.render(), [innerQtree])
-
-
-    def punt(self, stuff):
-        print "And(%s) punt(%s), extending lhs stack %s and rhs stack %s, my stack is %s" % (
-            self.render(), [s.render() for s in stuff],
-	    [s.render() for s in self.lhs.stack],
-	    [s.render() for s in self.rhs.stack],
-	    [s.render() for s in self.stack],
-        )
-        self.lhs.stack.extend(stuff)
-        self.rhs.stack.extend(stuff)
+    def sympy_me(self):
+        return sympy_me(lhs) & sympy_me(rhs)
 
 
 class PropVar(TraversibleExpression):
@@ -169,38 +108,14 @@ class PropVar(TraversibleExpression):
         TraversibleExpression.__init__(self)
         self.name = name
 
-
     def render(self):
         return self.name
 
-
-    def branch(self):
-        return r"%s" % (self.render())
-
-
-    def qtree(self, elideRoot=False):
-        """
-        Given the expression that is self, return a Qtree which can be rendered
-        into the LaTeX we want to output.
-        """
-        return Qtree(self.render())
-
-
-    def punt(self, stuff):
-        print "PropVar(%s) punt(%s), extending own stack %s" % (
-            self.render(), [s.render() for s in stuff],
-	    [s.render() for s in self.stack],
-        )
-        for s in stuff:
-            if s not in self.stack:
-                self.stack.append(s)
-            else:
-                print "FOUND A DUPE, IGNORING IT"
-        #self.stack.extend(stuff)
-
+    def sympy_me(self):
+      "returns a sympy Symbol"
+      return sympify(self.name)
 
 if __name__ == "__main__":
-    expr = And(Or(PropVar('p'), PropVar('q')), PropVar('r'))
-    print expr.render()
+    expr = And_exp(Or_exp(PropVar('p'), PropVar('q')), PropVar('r'))
+    print expr.sympy_me()
     # testing
-
--- tree_yacc2.py
+++ tree_yacc2.py
@@ -15,61 +15,49 @@ compound_prop:  | PROP_VAR
 import os
 
 import ply.yacc as yacc
-from tree import Or, Not, And, PropVar
+
+from tree import Or_exp, Not_exp, And_exp, PropVar
 
 from lexer import tokens, tokenise
 
+
 # The following functions are based on David Baezley's parser. The indices that are used in the functions correspond to the words, in order, in the doc strings.
+# The functions describe the relationship between different types of propositional formula, indicated by the BNF grammar above.
+# They convert the tokens into instances of the classes from tree.py
+
 
 def p_compound_prop_PROP_VAR(p):
     "compound_prop : PROP_VAR"
     p[0] = PropVar(p[1])
 
-# Here, p[0] is the result of calling PropVar on the 1st word (excluding the colon) in the doc string -- i.e. the PropVar itself.
-
 def p_compound_prop_NEG(p):
     "compound_prop : NEG compound_prop"
-    p[0] = Not(p[2])
-# p[0] is Not(compound_prop)    
+    p[0] = Not_exp(p[2])
 
 def p_compound_prop_AND(p):
     "compound_prop : LPAREN compound_prop AND compound_prop RPAREN"
-    p[0] = And(p[2], p[4])
+    p[0] = And_exp(p[2], p[4])
 
 def p_compound_prop_OR(p):
     "compound_prop : LPAREN compound_prop OR compound_prop RPAREN"
-    p[0] = Or(p[2], p[4])
+    p[0] = Or_exp(p[2], p[4])
 
 def p_compound_prop_ARROW(p):
     "compound_prop : LPAREN compound_prop ARROW compound_prop RPAREN"
-    p[0] = Or(Not(p[2]), p[4])
+    p[0] = Or_exp(Not_exp(p[2]), p[4])
 
 def p_compound_prop_IFF(p):
     "compound_prop : LPAREN compound_prop IFF compound_prop RPAREN"
-    p[0] = Or(And(p[2], p[4]), And(Not(p[2]), Not(p[4])))
+    p[0] = Or_exp(And_exp(p[2], p[4]), And_exp(Not_exp(p[2]), Not_exp(p[4])))
 
 
 # Error rule for syntax errors
 def p_error(p):
     print "Syntax error in input!"
 
-
-
 # Build the parser
 parser = yacc.yacc()
 
-
-# The tex code that prints a tree
-tex_str = r"""
-\documentclass[10pt,english]{article}
-\usepackage[T1]{fontenc}
-\usepackage[latin9]{inputenc}
-\usepackage{amssymb}
-\usepackage{qtree}
-\begin{document} 
-Tree:
-\Tree %s
-\end{document} """ 
 
 s1 = raw_input('first premiss > ')
 s2 = raw_input('second premiss > ')
@@ -84,37 +72,27 @@ def propvarfinder(string1, string2, conclusion):
               propvars.append(tok.value)
   return propvars
 
-def propvarevaluator(li):
-    values = {}
-    for propvar in li:
-        propvartruthvalue = raw_input('Truth value of %s ? Give me a \'T\' or \'F\' ' % propvar )
-        if propvartruthvalue not in ['T', 'F', 't', 'f']:
-            print "whoops! say 'T' or 'F'!"
-        else:
-            print 'you said %s !' % propvartruthvalue
-            if propvartruthvalue in ['T', 't']:
-                values.update({propvar: True})
-            else:
-                values.update({propvar: False})
-    print "Thank you! I have all the information I need"
-    return values
-
 propvarfinder(s1, s2, c)
-print propvarevaluator(propvarfinder(s1, s2, c))
 
 prem1 = parser.parse(s1)
 prem2 = parser.parse(s2)
 conclusion = parser.parse(c)
 print 'premises are %s and %s, conlusion is %s' % (s1, s2, c)
 
-print prem1
-print prem2
-print conclusion
-
-
 print 'these are your propvars:' 
 print propvarfinder(s1, s2, c)
 
+print "prem1 is a %s" % prem1
+print str(prem2)
 
+# The parser turns my raw input into Python objects from defined classes. 
+# Each class has a function for turning its instances into Sympy objects.
 
+"""
+if __name__=="__main__":
+    for k, v in globals().items():
+        print k, '=', v
+"""
+
+print "SYMPYING prem1!" + str(prem1.sympy_me())
 
