ab0251a487723c4ab6c5c088166a2da34dd8565a
Kate Hodesdon <kate@hodesdon.com>
2016-10-16 13:56:35 +0100

    adding support for LaTex rendering of tree proofs

--- tree.py
+++ tree.py
@@ -8,8 +8,8 @@ import copy
 
 class TraversibleExpression(object):
     """
-    An expression which knows how to generate its own tree for propositional
-    logic.
+    An expression in propositional logic that can generate a tree proof style tree
+    and generate a corresponding object using Python Sympy objects.
     """
     def __init__(self):
         self.stack = []
@@ -55,6 +55,20 @@ class Not_exp(TraversibleExpression):
         return "Not(%s)" % (self.sub.render(),)
         #return "NOT " + self.sub.render() 
   
+    def branch(self):
+        return r"%s" % (self.render())
+
+    def qtree(self, elideRoot=False):
+        return Qtree(self.render())
+
+    def punt(self, stuff):
+        print "Not(%s) punt(%s), extending own stack %s" % (
+            self.render(), 
+	    [s.render() for s in stuff], 
+	    [s.render() for s in self.stack],
+        )
+        self.stack.extend(stuff)
+
     def sympy_me(self):
         return ~(self.sub.sympy_me())
 
@@ -74,10 +88,26 @@ class Or_exp(TraversibleExpression):
     def render(self):
         return "Or(%s, %s)" % (self.lhs.render(), self.rhs.render())
         #return "(" + self.lhs.render() + " OR " + self.rhs.render() + ")"
-   
+
+    def qtree(self, elideRoot=False):
+        if elideRoot:
+            text = ""
+        else:
+            text = self.render()
+        return Qtree(text, [self.lhs.getSubTree(), self.rhs.getSubTree()])
+
+    def punt(self, stuff):
+        print "Or(%s) punt(%s), extending lhs stack %s and rhs stack %s, my stack is %s" % (
+            self.render(), [s.render() for s in stuff],
+	    [s.render() for s in self.lhs.stack],
+	    [s.render() for s in self.rhs.stack],
+	    [s.render() for s in self.stack],
+        )
+        self.lhs.stack.extend(stuff)
+        self.rhs.stack.extend(stuff)
+
     def sympy_me(self):
         return self.lhs.sympy_me() | self.rhs.sympy_me()
-
 
 class And_exp(TraversibleExpression):
     """
@@ -93,6 +123,39 @@ class And_exp(TraversibleExpression):
     def render(self):
         return "And(%s, %s)" % (self.lhs.render(), self.rhs.render())
 
+    def _newlineHead(self):
+        return "%s\n%s" % (self.lhs.render(), self.rhs.render())
+
+    def qtree(self, elideRoot=False):
+        """
+        For And expressions, render a subtree with each of the conjuncts
+        separated by a newline as the head, and then proceed to recursively
+        expand each conjunct in turn, one at a time.
+        """
+        if self.lhs.isComplex and self.rhs.isComplex:
+            self.lhs.stack.append(self.rhs)
+
+        subQtreeBranches = []
+        if self.lhs.isComplex:
+            subQtreeBranches.append(self.lhs.getSubTree(elideRoot=True))
+        elif self.rhs.isComplex:
+            subQtreeBranches.append(self.rhs.getSubTree(elideRoot=True))
+
+        innerQtree = Qtree(self._newlineHead(), subQtreeBranches)
+        if elideRoot:
+            return innerQtree
+        else:
+            return Qtree(self.render(), [innerQtree])
+
+    def punt(self, stuff):
+        print "And(%s) punt(%s), extending lhs stack %s and rhs stack %s, my stack is %s" % (
+            self.render(), [s.render() for s in stuff],
+	    [s.render() for s in self.lhs.stack],
+	    [s.render() for s in self.rhs.stack],
+	    [s.render() for s in self.stack],
+        )
+        self.lhs.stack.extend(stuff)
+        self.rhs.stack.extend(stuff)
     def sympy_me(self):
         return self.lhs.sympy_me() & self.rhs.sympy_me()
 
@@ -110,9 +173,33 @@ class PropVar(TraversibleExpression):
     def render(self):
         return self.name
 
-    def sympy_me(self):
+    def branch(self):
+        return r"%s" % (self.render())
+
+    def qtree(self, elideRoot=False):
+        """
+        Given the expression that is self, return a Qtree which can be rendered
+        into the LaTeX we want to output.
+        """
+        return Qtree(self.render())
+
+    def punt(self, stuff):
+        print "PropVar(%s) punt(%s), extending own stack %s" % (
+            self.render(), [s.render() for s in stuff],
+	    [s.render() for s in self.stack],
+        )
+        for s in stuff:
+            if s not in self.stack:
+                self.stack.append(s)
+            else:
+                print "FOUND A DUPE, IGNORING IT"
+        #self.stack.extend(stuff)
+
+
+
+    def sympy_me(
       "returns a sympy Symbol"
-      return sympify(self.name)
+      return ify(self.name)
 
 if __name__ == "__main__":
     expr = And_exp(Or_exp(PropVar('p'), PropVar('q')), PropVar('r'))
